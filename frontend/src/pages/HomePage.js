d3d2c2b295c69ec9b964af070461dcccd6faede7
Dan <daniel.liemng@gmail.com>
2022-01-11 03:01:28 -0500

    UI: Login/Sign up

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,58 @@
+import {
+  Box,
+  Container,
+  Text,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from '@chakra-ui/react';
+import Login from '../components/authentication/Login';
+import Signup from '../components/authentication/Signup';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW='xl'>
+      <Box
+        d={'flex'}
+        justifyContent={'center'}
+        p={3}
+        bg={'white'}
+        w={'100%'}
+        m={'40px 0 15px 0'}
+        borderRadius={'lg'}
+        borderWidth={'1px'}
+      >
+        <Text fontSize={'4xl'} fontFamily={'Work sans'} color={'black'}>
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box
+        bg={'white'}
+        color={'black'}
+        w={'100%'}
+        p={4}
+        borderRadius={'lg'}
+        borderWidth={'1px'}
+      >
+        <Tabs variant='soft-rounded'>
+          <TabList mb={'1em'}>
+            <Tab width={'50%'}>Login</Tab>
+            <Tab width={'50%'}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
