da10c3f8f8298cf90e91246e65f3fd01d4cc5f8e
Dan <daniel.liemng@gmail.com>
2022-01-11 01:26:08 -0500

    frontend setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,24 @@ require('dotenv').config();
 
 const express = require('express');
 
+const { chats } = require('./data/data');
+
 const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('OKKK');
+});
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+  const oneChat = chats.find((item) => item._id === req.params.id);
+  console.log(oneChat);
+  res.send(oneChat);
+});
 
 const PORT = process.env.PORT || 5001;
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ChatPage from './pages/ChatPage';
+
 const App = () => {
-  return <div className='App'>App</div>;
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/chats' element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
