e7596688275c8d64d52509dc5f578b8898f2d4ab
Dan <daniel.liemng@gmail.com>
2022-01-12 00:35:19 -0500

    upload profile image to cloudinary

--- frontend/src/components/authentication/Signup.js
+++ frontend/src/components/authentication/Signup.js
@@ -6,8 +6,10 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from '@chakra-ui/react';
 import { useState } from 'react';
+import axios from 'axios';
 
 const Signup = () => {
   const [show, setShow] = useState(false);
@@ -17,14 +19,60 @@ const Signup = () => {
   const [password, setPassword] = useState('');
   const [confirmpassword, setConfirmpassword] = useState('');
   const [pic, setPic] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const toast = useToast();
 
   const handleShowPassword = () => {
     setShow((prevState) => !prevState);
   };
 
-  const postDetails = (pic) => {};
+  const postPicture = async (pic) => {
+    setLoading(true);
+    if (pic === undefined) {
+      toast({
+        title: 'Please select an Image!',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'top',
+      });
+      return;
+    }
+
+    if (pic.type === 'image/jpeg' || pic.type === 'image/png') {
+      const picData = new FormData();
+      picData.append('file', pic);
+      picData.append('upload_preset', 'advanced_chat_2022');
+      picData.append('cloud_name', 'danhln');
+
+      try {
+        const { data } = await axios.post(
+          `https://api.cloudinary.com/v1_1/danhln/image/upload`,
+          picData,
+        );
+        setPic(data.url.toString());
+        setLoading(false);
+      } catch (err) {
+        console.log(err.message);
+        setLoading(false);
+      }
+    } else {
+      toast({
+        title: 'Please select an Image!',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'top',
+      });
+      setLoading(false);
+      return;
+    }
+  };
 
   const submitHandler = (e) => {};
+
+  console.log('DDD', pic);
 
   return (
     <VStack spacing={'5px'}>
@@ -83,7 +131,7 @@ const Signup = () => {
           type={'file'}
           p={'1.5'}
           accept='image/*'
-          onChange={(e) => postDetails(e.target.files[0])}
+          onChange={(e) => postPicture(e.target.files[0])}
         />
       </FormControl>
 
@@ -92,6 +140,7 @@ const Signup = () => {
         width={'100%'}
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Sign Up
       </Button>
